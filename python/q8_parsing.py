521658f9e3ae2506f95c7b14a27c6030e3d3dad9
Chris L Buie <chrisbuie@gmail.com>
2016-08-26 09:31:55 -0500

    update of list.py, not complete

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,29 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+
+def load_mydata (fin,feature):
+    import csv
+    reader = csv.DictReader(open(fin))
+    r = {}
+    for row in reader:
+        key = row.pop(feature)
+        if key in r:
+            # implement your duplicate row handling here
+            pass
+        r[key] = row
+    return r
+
+
+def find_min(data,f1,f2):
+    ml = []
+    for i in data:
+        x = i, int(data[i]['Goals']) - int(data[i]['Goals Allowed'])
+        ml.append(x)
+    ml = sorted(ml, key=lambda tup: tup[1])
+    return ml[0]
+
+
+data = load_mydata('python/football.csv','Team')
+result = find_min(data,'Goals','Goals Allowed')
+print result
