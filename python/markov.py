8cf7962daed8a6c6c92a3aa461544eb4cf6d72c1
Chris L Buie <chrisbuie@gmail.com>
2016-08-29 10:48:29 -0500

    update of list.py, not complete

--- python/markov.py
+++ python/markov.py
@@ -11,9 +11,34 @@
 # > show himself once more than the universe and what I often catch myself playing our well-connected game went on. Our friend was absolutely correct: nobody from the group needed this way. We never been as the Earth has the network of eternity.
 
 # There are design choices to make; feel free to experiment and shape the program as you see fit. Jeff Atwood's [Markov and You](http://blog.codinghorror.com/markov-and-you/) is a fun place to get started learning about what you're trying to make.
+import re
 
-> REPLACE THIS TEXT WITH YOUR PROGRAM
-import csv
 
-reader = csv.reader(open('python/Beyond Good and Evil'))
+fin = 'python/Beyond Good and Evil'
 
+def word_list(fin):
+    word_list = []
+    with open(fin,'r') as fit:
+        for line in fit:
+            for word in line.split():
+                word_list.append(word)
+    return word_list
+
+
+def word_dict(words):
+    d = {}
+    for i, word in enumerate(words):
+        try:
+            first, second, third = words[i], words[i+1], words[i+2]
+        except IndexError:
+            break
+        key = (first, second)
+        if key not in d:
+            d[key] = []
+        #
+        d[key].append(third)
+
+    return d
+
+words = word_list(fin)
+word_tups = word_dict(words)
